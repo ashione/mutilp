8e1f13f353b04f85462a6870b7aa306d1505385b
ashione <skyzlxuan@gmail.com>
2015-04-09 22:12:36 +0800

    throw into src

---
+++ src/mutiltest.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+from Distribution import Distribution
+if __name__ == '__main__':
+#    print 'No errors'    
+    f=open('slaves','r') 
+    nodes = [ node.replace('\n','') for node in f.readlines()]
+    f.close()
+
+    print nodes
+    terminalPath = 'mutilprocessHW'
+    jobs = nodes 
+    driver = 'mutilqueue.py'
+    print jobs
+    dt = Distribution(driver = driver,jobs=jobs,nodes=nodes,terminalPath=terminalPath,nprocess = 4)
+    dt.nohupdisbale()
+    dt.cleanJobDriver()
+    dt.distributionJobDriver()
+    dt.run()
