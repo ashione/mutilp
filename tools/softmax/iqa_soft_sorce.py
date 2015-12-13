5ff1e3216bedac3e89b7f1877ce0858530984f59
tj <tj@node08.(none)>
2015-12-13 16:41:18 +0800

    add distortion recgnition

--- tools/softmax/iqa_soft_sorce.py
+++ tools/softmax/iqa_soft_sorce.py
@@ -11,6 +11,7 @@ def readlines(filename):
     rawdata = f.readlines()
     f.close()
     return rawdata
+
 def cleanLineorR(line):
     p = re.compile(r'\r|\n')
     return p.sub('',line)
@@ -53,7 +54,9 @@ def getListFromDict(adict,classNum):
     return alist
 
 def argmaxFromSoftmax(ps):
-    data = map(lambda x: map(lambda t : float(t),filter(lambda u :u!='',x.replace('\n','').split(' '))),readlines(ps.s)[2::3])
+    linesdata = readlines(ps.s)[2::3]
+    print 'read linesdata ok'
+    data = map(lambda x: map(lambda t : float(t),filter(lambda u :u!='',x.replace('\n','').split(' '))),linesdata)
     glog.info('Data Length : '+str(len(data)))
     data = np.array(data)
     data.shape = (data.size/ps.n,ps.n)
@@ -207,9 +210,123 @@ def iqa_soft_type(ps):
     for x in imagesSet :
         rlist.append((x,rdict[x][1]/rdict[x][0]))
     fp = open(ps.d,'w') 
-    map(lambda x : fp.write('%s %.5f\n'%(x[0],x[1])),rlist)
+    map(lambda x : fp.write('%s,%.5f\n'%(x[0],x[1])),rlist)
     fp.close()
         
+def iqa_soft_level(ps):
+    glog.info('Now couting level:'+ps.s)
+
+    classNum = ps.n 
+    linesdata = readlines(ps.l)
+    label = map(lambda x : int(x.replace('\n','').split(' ')[-1]),linesdata)
+    imagesName = map(lambda x : '_'.join(x.replace('\n','').split(' ')[-2].split('/')[-1].split('_')[:-1]),linesdata)
+    argmax = np.loadtxt(ps.s,dtype=np.int)
+
+    if ps.database == 'live_tid08':
+        imagesType = np.array(map(lambda x : int(x[1:3])-1,imagesName))
+        pos_live_tid08 = zip(argmax,imagesType)
+        imagesTypeDict = {}
+        for i in range(0,779):
+            if i in range(0,169):
+                imagesTypeDict[i] = 10
+            elif i in range(169,344):
+                imagesTypeDict[i] = 9
+            elif i in range(344,489):
+                imagesTypeDict[i] = 0
+            elif i in range(489,633):
+                imagesTypeDict[i] =7 
+            else :
+                imagesTypeDict[i] = -1
+
+    mos = np.loadtxt(ps.slev)
+    c_mos = np.loadtxt(ps.clev)
+    fmos = np.zeros(shape=c_mos.shape)
+    nmos = np.zeros(shape=c_mos.shape)
+    c_fmos = np.zeros(shape=c_mos.shape)
+    c_nmos = np.zeros(shape=c_mos.shape)
+    #argmax = map(lambda x : x%120/5,argmax)
+    if ps.database == 'live' or ps.database == 'live_tid08':
+        datamap = np.loadtxt('./label/LIVE_distortion_type.txt')
+
+    pos =  zip(argmax,label)
+    if ps.database == 'live_tid08':
+        pos = filter(lambda x : x[1]%68/4 in [10,9,0,7],pos) 
+    print len(pos)
+    #print pos
+    ac = sum(map(lambda x : 1.0 if x[0] == x[1] else 0 ,pos))
+    print ac/len(pos)
+
+    if ps.database =='tid08':
+        lac = sum(map(lambda x : 1.0 if x[0]%68/4 == x[1]%68/4 else 0 ,pos))
+    elif ps.database == 'tid13':
+        lac = sum(map(lambda x : 1.0 if x[0]%120/5 == x[1]%120/5 else 0 ,pos))
+    elif ps.database == 'live':
+        lac = sum(map(lambda x : 1.0 if datamap[x[0]] == datamap[x[1]] else 0 ,pos))
+    elif ps.database == 'live_tid08':
+        lac = sum(map(lambda x : 1.0 if imagesTypeDict[x[0]] == x[1]%68/4 else 0 ,pos))
+
+    print lac/len(pos)
+    for i,item in enumerate(pos) :
+        #glog.info('argmax : {}, label : {}'.format(argmax[i],item))
+        #glog.info('{},{}'.format(imagesTypeDict[argmax[i]],imagesType[i]))
+        nmos[item[1]]+=1.0
+        fmos[item[1]]+=mos[item[0]]
+
+        #print imagesTypeDict[item[0]],item[1]%68/4
+        if (ps.database == 'tid13' and item[1]%120/5 == item[0]%120/5 ) or \
+           (ps.database == 'tid08' and item[1]%68/4 == item[0]%68/4) or \
+           (ps.database == 'live' and datamap[item[1]] == datamap[item[0]]) or\
+           (ps.database == 'live_tid08' and imagesTypeDict[item[0]] == item[1]%68/4):
+            c_nmos[item[1]]+=1.0
+            c_fmos[item[1]]+=mos[item[0]]
+           
+    c_index = (nmos!=0.0)
+    c_mos = c_mos[c_index]
+    result = np.divide(fmos[c_index],nmos[c_index]) 
+    c_findex = c_nmos!=0.0
+    c_result = np.divide(c_fmos[c_findex],c_nmos[c_findex]) 
+    #glog.info('{}'.format(nmos[c_index].shape))
+    print result.shape,c_result.shape
+    #glog.info('result : {}'.format(result))
+    #glog.info('c_mos : {}'.format(c_mos))
+    #print fmos
+    #print nmos
+    #print c_mos
+    plcc(result,c_mos)
+    glog.info('spearmanr : {0}'.format(scipy.stats.spearmanr(result,c_mos)))
+    plcc(c_result,c_mos)
+    glog.info('spearmanr : {0}'.format(scipy.stats.spearmanr(c_result,c_mos)))
+    np.savetxt(ps.d,result,fmt='%.5f')
+    np.savetxt(ps.d.replace('.csv','_deep.csv'),c_result,fmt='%.5f')
+    #np.save('temp.txt',np.array(pos))
+#    if ps.c :
+#        sdict = generateScoreDictByRawData(ps.t)
+#    else:
+#        sdict = genereteSocreByData(ps.t)
+#
+#    for k,v in sdict.items():
+#        if v == 0:
+#            sdict[k]= 0.0
+ 
+##print sdict.items()
+#
+#    rdict = collections.defaultdict(lambda :(0,0.0) )
+#
+#    rdict_inner = collections.defaultdict(lambda :(0,0.0) )
+##sdict[0] = 0.0
+#
+#    for i,x in enumerate(pos ):
+#       # print x
+#        rdict[imagesName[i]]= (rdict[imagesName[i]][0]+1,rdict[imagesName[i]][1]+sdict[int(x[0])]) 
+#    imagesSet = set(imagesName)
+#    rlist = []
+#    for x in imagesSet :
+#        rlist.append((x,rdict[x][1]/rdict[x][0]))
+#    fp = open(ps.d,'w') 
+#    map(lambda x : fp.write('%s,%.5f\n'%(x[0],x[1])),rlist)
+#    fp.close()
+#        
+#
 
 #print max(argmax)
 if __name__ == '__main__' :
@@ -220,11 +337,17 @@ if __name__ == '__main__' :
     p.add_argument('-n',type=int,help="class num")
     p.add_argument('-c', action="store_true", default=False)
     p.add_argument('-d', help="store file", default="iqa_sorce.csv")
+    p.add_argument('-slev', help="store file", default="iqa_sorce.txt")
+    p.add_argument('-clev', help="store file", default="iqa_sorce.txt")
+    p.add_argument('-database', help="database file", default="tid13")
 
     p.add_argument('-q', action="store_true", default=False) 
     p.add_argument('-type',action="store_true",default=False)
+    p.add_argument('-level',action="store_true",default=False)
     ps = p.parse_args() 
-    if not ps.type :
+    if ps.level:
+       iqa_soft_level(ps) 
+    elif not ps.type :
         iqa_soft_score(ps)
     else :
         iqa_soft_type(ps)
